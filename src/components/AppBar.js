d394882a2ffc97da7ead5ce04327d65728ae16a5
Achourbenakli <achourbenakli@yahoo.fr>
2021-08-10 21:19:25 +0200

    update

---
+++ src/components/AppBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Input } from 'react-native-elements';
+
+
+const AppBar = () => {
+  return (
+    <>
+      <Input placeholder='BASIC INPUT' />
+    </>
+  )
+}
+
+export default AppBar
